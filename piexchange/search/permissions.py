a7345a0fc07afb2ff8483b49d4d34cb42adc927b
Santosh Patil <Santosh.Patil@sportsbet.com.au>
2020-02-18 17:02:58 +1100

    Refactored some code

--- piexchange/search/permissions.py
+++ piexchange/search/permissions.py
@@ -11,7 +11,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     Custom permission to only allow owners of an object to edit it.
     """
 
-    def has_object_permission(self, request, obj):
+    def has_object_permission(self, request, view, obj):
         """
         Read permissions are allowed to any request,
         so we'll always allow GET, HEAD or OPTIONS requests.
